9dbe2f05a16689532ade5d59518771c5c51365a2
itssct <Thomas.Schmauder@bib.de>
2019-05-22 12:15:40 +0200

    First Commit

---
+++ Gittest/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  Gittest
+//
+//  Created by XYZ - Dozent on 5/22/19.
+//  Copyright © 2019 XYZ - Dozent. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Networking Code")
+    }
+
+
+}
+
---
+++ Gittest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Gittest
+//
+//  Created by XYZ - Dozent on 5/22/19.
+//  Copyright © 2019 XYZ - Dozent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
